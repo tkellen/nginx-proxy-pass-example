3e921e0849ae795745261dfba1c533243ac79580
Tyler Kellen <tyler@sleekcode.net>
2016-03-01 16:50:59 -0500

    fix app again

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const host = process.env.HOST || '0.0.0.0';
 const http = require('http');
 
 function handler (request, response) {
-  response.send(request.headers);
+  response.end(JSON.stringify(request.headers,null,2));
 }
 
 http.createServer(handler).listen(port, function(){
